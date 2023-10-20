4d5527b14b23355654f5da07aaf5e8573eb5516d
burno <A00838627@tec.mx>
2023-10-20 14:01:29 -0600

    Final version

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
 from modules.pokemon import Pokemon
 from modules.button import button
+from modules.preguntas import preguntas
 from random import choice, shuffle
-from preguntas import preguntas
 from os import system
+from time import sleep
 
 system("clear")
 
@@ -14,23 +15,28 @@ height = 200
 
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Battle")
-
+ 
 q = list(preguntas.keys())
 shuffle(q)
 p = []
 
-running = True
+running = False
 inicio_pressed = False
+back_pressed = False
 win = False
 lose = False
+d = True
+dif = 1
 
 battle_background = pygame.image.load("images/backgrounds/battle_background.png")
+pantalla_dificultad = pygame.image.load("images/backgrounds/pantalla_dificultad.png")
 victory = pygame.image.load("images/backgrounds/victory.png")
 defeat = pygame.image.load("images/backgrounds/defeat.png")
 back_button = pygame.image.load("images/buttons/back.png")
 iniciar_button = pygame.image.load("images/buttons/iniciar.png")
 salir_button = pygame.image.load("images/buttons/salir.png")
 restart_button = pygame.image.load("images/buttons/restart.png")
+dificultad_button = pygame.image.load("images/buttons/dificultad.png")
 made_by = pygame.image.load("images/text/made_by.png")
 squirtle_hp = pygame.image.load("images/text/squirtle_hp.png")
 charmander_hp = pygame.image.load("images/text/charmander_hp.png")
@@ -38,18 +44,60 @@ charmander_hp = pygame.image.load("images/text/charmander_hp.png")
 iniciar = button(75, 25, iniciar_button)
 salir_menu = button(90, 100, salir_button)
 salir_juego = button(90, 140, salir_button)
+dificultad = button(90, 85, dificultad_button)
 reiniciar = button(90, 90, restart_button)
 back = button(10, 10, back_button)
+
+
+while d:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            d = False
+
+    if not back_pressed:
+        screen.blit(pantalla_dificultad, (0, 0))
+        lvl = pygame.image.load(f"images/backgrounds/{dif}.png")
+        screen.blit(lvl, (0, 130))
+        screen.blit(made_by, (90, 170))
+
+        if dificultad.draw(screen):
+            if dif == 3:
+                dif = 1
+            else:
+                dif += 1
+
+    if back.draw(screen):
+        running = True
+        d = False
+
+    pygame.display.update()
+
+s_hp = 0
+c_hp = 0
+
+if dif == 1:
+    s_hp = 3
+    c_hp = 6
+
+elif dif == 2:
+    s_hp = 2
+    c_hp = 7
+
+elif dif == 3:
+    s_hp = 1
+    c_hp = 9
+
 
 squirtle_sprite = pygame.image.load("images/sprites/squirtle.png")
 squirtle_fainted_sprite = pygame.image.load("images/sprites/squirtle_fainted.png")
 
-squirtle = Pokemon("Squirtle", 3, False, squirtle_sprite, squirtle_fainted_sprite, 20, 110)
+squirtle = Pokemon("Squirtle", s_hp, False, squirtle_sprite, squirtle_fainted_sprite, 20, 110)
 
 charmander_sprite = pygame.image.load("images/sprites/charmander.png")
 charmander_fainted_sprite = pygame.image.load("images/sprites/charmander_fainted.png")
 
-charmander = Pokemon("Charmander", 6, False, charmander_sprite, charmander_fainted_sprite, 170, 0)
+charmander = Pokemon("Charmander", c_hp, False, charmander_sprite, charmander_fainted_sprite, 170, 0)
+
 
 while running:
     for event in pygame.event.get():
@@ -94,22 +142,28 @@ while running:
             respuesta = input("Respuesta: ")
 
             if respuesta in preguntas[pregunta]:
+                print("\n¡Correcto!")
                 charmander.lose_health()
+                print("\n")
 
             else:
+                print("\n¡Incorrecto!")
                 squirtle.lose_health()
+                print("\n")
 
         if squirtle.hp == 0:
             screen.blit(defeat, (0, 0))
             lose = True
 
             if reiniciar.draw(screen):
-                squirtle.hp = 3
-                charmander.hp = 6
+                squirtle.hp = s_hp
+                charmander.hp = c_hp
                 squirtle.sprite = squirtle_sprite
                 p = []
                 lose = False
                 inicio_pressed = False
+                system("clear")
+                sleep(0.5)
 
             if salir_juego.draw(screen):
                 running = False   
@@ -125,6 +179,7 @@ while running:
                 p = []
                 win = False
                 inicio_pressed = False
+                sleep(0.5)
 
             if salir_juego.draw(screen):
                 running = False
@@ -132,3 +187,5 @@ while running:
         pygame.display.update()
         
     pygame.display.update()
+
+system("clear")
---
+++ modules/preguntas.py
@@ -0,0 +1,11 @@
+preguntas = {
+    "El perímetro de un cuadrado es de 80 cm. ¿Cuál es su área?" : ["400"],
+    "Un tren viaja a una velocidad de 120 km/h. ¿Cuántos segundos tardará en recorrer 500 km?": ["15000"],
+    "Una mezcla de agua y alcohol contiene 30% de alcohol. ¿Cuántos litros de alcohol hay en 10 litros de la mezcla?": ["3"],
+    "Un rectángulo tiene una base de 12 cm y un área de 30 cm. ¿Cuál es su altura?": ["5"],
+    "¿Cuál es el resultado de la siguiente operación?\n (2x + 3) - (x - 1) = ?": ["x + 4"],
+    "¿Cuál es una posible solución para la siguiente ecuación?\n x² + 4x - 5 = 0": ["1", "-5"],
+    "¿Cuál es el valor de x en la siguiente ecuación?\n 2x - 3 = 9": ["6"],
+    "¿Cuál es el resultado de la siguiente operación?\n (2 + 3) / 5 * 4 = ?": ["4"],
+    "¿Cuál es la ecuación cuadrática que tiene las soluciones x = 2 y x = -3?": ["(x-2)*(x+3)=0", "(x-2)(x+3), (x-2)*(x+3), x²+x-6=0, x²+x-6"]
+}
